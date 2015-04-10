e7824dda0b4f5d7b84db0f118cacbeb86cf201f1
大島 曜 <ohshima@ooshima-no-mini.lan>
2015-04-10 20:46:38 +0900

    初めてのコミット

---
+++ SandBox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SandBox
+//
+//  Created by 大島 曜 on 2015/04/10.
+//  Copyright (c) 2015年 大島 曜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SandBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SandBox
+//
+//  Created by 大島 曜 on 2015/04/10.
+//  Copyright (c) 2015年 大島 曜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
