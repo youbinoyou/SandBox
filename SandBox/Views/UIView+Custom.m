9a60fcc47d85db59cba576a8963e6121b26294bb
大島 曜 <youbinoyou@gmail.com>
2015-05-04 13:28:50 +0900

    [File]UIViewカテゴリ（Custom）の追加

---
+++ SandBox/Views/UIView+Custom.m
@@ -0,0 +1,21 @@
+//
+//  UIView+Custom.m
+//  SandBox
+//
+//  Created by 大島 曜 on 2015/05/04.
+//  Copyright (c) 2015年 大島 曜. All rights reserved.
+//
+
+#import "UIView+Custom.h"
+
+@implementation UIView(Custom)
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
