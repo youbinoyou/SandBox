3ac7976d7a0444c9cabc85fd10f67ca7259eef98
大島 曜 <youbinoyou@gmail.com>
2015-05-04 11:41:38 +0900

    [FileAdd]UIAlertにCustomカテゴリを追加する

---
+++ SandBox/Views/UIAlertView+Custom.m
@@ -0,0 +1,21 @@
+//
+//  UIAlertView+Custom.m
+//  SandBox
+//
+//  Created by 大島 曜 on 2015/05/04.
+//  Copyright (c) 2015年 大島 曜. All rights reserved.
+//
+
+#import "UIAlertView+Custom.h"
+
+@implementation UIAlertView(Custom)
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
